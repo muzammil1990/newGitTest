df101f2b91ec1b5c5e3247061aff9e53bb40528e
muzammil1990 <muzammil.c1990@gmail.com>
2020-05-27 12:47:07 +0530

    added how feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ Class Hello{
    public stactic void main(){
       System.out.println("Hello World");
       System.out.println("Pretty Good");
+      
+      System.out.println("How do you do");
   }
 }
